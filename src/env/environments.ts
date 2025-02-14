fb917a79ed4b45569c6587dfbed3c294ecf2ff1f
Aszurar <lms.souza39@gmail.com>
2025-02-14 17:01:44 -0300

    Adicionar enums para ambientes e melhorar a configuração de variáveis de ambiente

---
+++ src/env/environments.ts
@@ -0,0 +1,12 @@
+enum Environment {
+  TEST = 'test',
+  DEV = 'development',
+  PRODUCTION = 'production',
+}
+
+enum EnvironmentFilesName {
+  TEST = '.env.test',
+  DEV = '.env',
+}
+
+export { Environment, EnvironmentFilesName }
--- src/env/index.ts
+++ src/env/index.ts
@@ -1,26 +1,43 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { z } from 'zod'
+import { Environment, EnvironmentFilesName } from './environments'
 
-enum Environment {
-  TEST = 'test',
-  DEV = 'development',
-  PRODUCTION = 'production',
+if (process.env.NODE_ENV === Environment.TEST) {
+  config({
+    path: EnvironmentFilesName.TEST,
+  })
+} else {
+  config()
 }
+
+console.log("process.env.DATABASE_URL", process.env.DATABASE_URL)
 
 const envSchema = z.object({
   NODE_ENV: z
-    .enum([Environment.DEV, Environment.TEST, Environment.PRODUCTION])
+    .enum([Environment.DEV, Environment.TEST, Environment.PRODUCTION], {
+      message: 'Invalid NODE_ENV',
+    })
     .default(Environment.DEV),
-  DATABASE_URL: z.string().trim().min(1),
+  DATABASE_URL: z.string({
+    required_error: 'DATABASE_URL env is required',
+  }).trim().min(1),
   PORT: z.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
 
 if (!_env.success) {
-  console.error('❌ Invalid environment variables:', _env.error.format())
 
-  throw new Error('Invalid environment variables')
+  const skipLine = '\n'
+  const title = `❌ Check your envs file - Environment variables validation failed ${skipLine}`
+
+  const description = _env.error.errors
+    .map((error, index) => `${index} - ${error.message}`)
+    .join(`${skipLine}${skipLine}`) + skipLine
+
+  const errorMessage = title + description
+
+  throw new Error(errorMessage)
 }
 
 export const env = _env.data
